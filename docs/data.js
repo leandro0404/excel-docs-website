4f436250ba627ad854d2e27de14fe2f713e2d053
Leandro Silveira Alves Moreira <leandrobhmgg@gmail.com>
2023-06-26 18:48:53 -0300

    data

---
+++ docs/data.js
@@ -0,0 +1,69 @@
+var tabs = [
+    {
+      tabName: 'Person',
+      fileName: 'person',
+      columns: [
+        'Dado',
+        'Natureza',
+        'Obrigatório',
+        'Produto',
+        'Necessidade',
+        'País',
+        'Momento',
+        'Local',
+        'Tabela',
+        'Plataforma',
+        'Hub',
+        'Display',
+        'Sparkle',
+        'Club',
+        'Checkout',
+        'Zendesk'
+      ]
+    },
+    {
+      tabName: 'Address',
+      fileName: 'address',
+      columns: [
+        'Dado',
+        'Natureza',
+        'Obrigatório',
+        'Produto',
+        'Necessidade',
+        'País',
+        'Momento',
+        'Local',
+        'Tabela',
+        'Plataforma',
+        'Hub',
+        'Display',
+        'Sparkle',
+        'Club',
+        'Checkout',
+        'Zendesk'
+      ]
+    },
+    {
+      tabName: 'Phone',
+      fileName: 'phone',
+      columns: [
+        'Dado',
+        'Natureza',
+        'Obrigatório',
+        'Produto',
+        'Necessidade',
+        'País',
+        'Momento',
+        'Local',
+        'Tabela',
+        'Plataforma',
+        'Hub',
+        'Display',
+        'Sparkle',
+        'Club',
+        'Checkout',
+        'Zendesk'
+      ]
+    }
+  ];
+  
